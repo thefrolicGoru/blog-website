0a519986b6462f048b46831681682f39f13bdf7d
Gaurav Goru <gorubadgujar@gmail.com>
2022-07-06 18:16:18 +0530

    Added port as var

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 const dbURI = 'mongodb+srv://goru:test1234@backend-blog-web.zjfh9nt.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI)
-    .then((result) => app.listen(3000,()=>{
-        console.log('Server running on port 3000')
+    .then((result) => app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}`)
     }))
     .catch((err) => console.log(err));
 
